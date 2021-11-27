b4f2839139ededee729819285f8183e53fbb7e77
Gabriel de andrade silva <gabriel.andrade2402@gmail.com>
2021-11-27 13:44:47 -0300

    Open project

---
+++ API/codes/APIv8Baterias/src/test/java/com/apiGeneralV8/APIv8Baterias/ApIv8BateriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiGeneralV8.APIv8Baterias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIv8BateriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
