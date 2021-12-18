a20214027493228b5ac8aa476104a60d25926183
Gabriel de andrade silva <gabriel.andrade2402@gmail.com>
2021-12-18 13:41:52 -0300

    Escopo principal do header home

--- website/src/pages/Home/index.tsx
+++ website/src/pages/Home/index.tsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
-      <header> 
-        <div className="container row">
+      <header className='container-flex position-flex'> 
+        <div className=" row shadow bg-white rounded">
             <div className="col-sm-4">
                 <div className="col-sm-8 m-2">
-                    <label className="col-12 text-uppercase text-nowrap font-weight-bold mt-2">Email</label>
+                    <label className="col-12 text-lowercase text-nowrap font-weight-bold mt-2">Email</label>
                     <input type="text" name="email" id="email" className="col-12 ml-3"/>
                 </div>
                 <div className="col-sm-8 m-2">
-                    <label className="col-12 text-uppercase text-nowrap font-weight-bold mt-2">Password</label>
+                    <label className="col-12 text-lowercase text-nowrap font-weight-bold mt-2">Password</label>
                     <input type="password" name="senha" id="senha" className="col-12 ml-3"/>
                 </div>
                 <Link className="col-sm-3 m-2 btn btn-dark" to="/">
@@ -22,12 +22,12 @@ const Home = () => {
                     Cadastrar-se
                 </Link>
             </div>
-            <div className="col-sm-5 mt-3 justify-content-center align-items" >
-                <Link className="col-12 ml-3 mr-3" to="/">
-                <img src={Logomarca} alt="V8 Baterias" width='250' />
+            <div className="col-sm-4 m-3" >
+                <Link className="col-12 " to="/">
+                <img src={Logomarca} alt="V8 Baterias" />
                 </Link>
             </div>
-            <div className="col-sm-3 mt-3">
+            <div className="col-sm-3 mt-5 ml-5">
                 <Link className="btn btn-warning btn-lg col-12 m-2" to="/loja">
                     Loja
                 </Link>
