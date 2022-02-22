1962acb9fc425b3c0ef6a518d0953754ac8bdb1a
Gabriel-Andrade2402 <gabriel.andrade2402@gmail.com>
2022-02-22 20:53:55 -0300

    teste consumo api

--- website/src/pages/Home/index.tsx
+++ website/src/pages/Home/index.tsx
@@ -9,8 +9,34 @@ import HomeAppMore from '../../components/HomeComp/HomeAppMore';
 import HomeSkills from '../../components/HomeComp/HomeSkills';
 import '../../assets/css/home.css';
 import $ from 'jquery';
+import react,{ useState,useEffect } from 'react';
+import axios from "axios";
+import { BASE_URL } from "../../utils/requests";
 
 const Home = () => {
+    useEffect(() => {
+      teste();
+    }, []);
+    function teste(){
+      axios({
+          method: "post",
+          url: "https://api.mercadopago.com/users/test_user",
+          data: {
+            site_id:"MLB"
+          },
+          headers: {
+              "Content-Type": "application/json",
+              "Authorization":"Bearer TEST-5733289434620462-021313-2d8589e80ce978cc5111abf8e9ec51d1-1073610524",
+              "Access-Control-Allow-Origin":"*"
+          },
+      })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (response) {
+          console.log(response);
+          });
+  }
     return (
        <>
          <HomeHeader/>
