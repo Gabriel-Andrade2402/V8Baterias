01300f5aadc64b043160ff19765924557bf713ec
Gabriel-Andrade2402 <gabriel.andrade2402@gmail.com>
2022-02-22 21:09:45 -0300

    teste consumo api

--- API/codes/APIv8Baterias/src/main/java/com/apiGeneralV8/APIv8Baterias/Pojo/PersonPojo.java
+++
@@ -1,8 +0,0 @@
-package com.apiGeneralV8.APIv8Baterias.Pojo;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class PersonPojo {
-	
-}
--- API/codes/APIv8Baterias/src/main/java/com/apiGeneralV8/APIv8Baterias/enums/RolesConfig.java
+++
@@ -1,6 +0,0 @@
-package com.apiGeneralV8.APIv8Baterias.enums;
-
-public class RolesConfig {
-	public static final String ROLE_ADMIN = "ROLE_ADMIN";
-    public static final String ROLE_CLIENT = "ROLE_CLIENT";
-}
